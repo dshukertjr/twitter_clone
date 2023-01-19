0f239f8b891325358e7062f8ef3695f8b4898434
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2023-01-19 11:45:59 +0900

    fix elevated button theme for dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -69,6 +69,18 @@ class MyApp extends ConsumerWidget {
             fontSize: 20,
           ),
         ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all<OutlinedBorder>(
+              const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+            ),
+            padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+              const EdgeInsets.symmetric(horizontal: 12, vertical: 0),
+            ),
+          ),
+        ),
       ),
       home: _page(appAuthState),
     );
